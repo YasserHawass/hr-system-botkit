64e14baf3ad38b9cee08290cec3e32b423252768
Yasser Hawass <yasser.hawass@gmail.com>
2020-04-03 19:32:17 +0200

    first dialogue

--- features/other.js
+++ features/other.js
@@ -3,6 +3,22 @@ module.exports = function(controller) {
     controller.hears('hello','message',async(bot, message) => {
     // do something!
     await bot.reply(message, 'Hello human')
-});
+    });
 
+    controller.hears('menu', 'message,direct_message', async(bot, message) => { 
+
+        await bot.reply(message, {
+            text: 'Here is a menu!',
+            quick_replies: [
+                {
+                    title: "Main",
+                    payload: "main-menu",
+                },
+                {
+                    title: "Help",
+                    payload: "help"
+                }
+            ]
+        });
+    });
 }
