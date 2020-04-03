448b9fc1a27a710b70cd364c1cb662e3d07b3424
Yasser Hawass <yasser.hawass@gmail.com>
2020-04-03 19:11:10 +0200

    additional script

---
+++ features/other.js
@@ -0,0 +1,8 @@
+// listen for a message containing the word "hello", and send a reply
+module.exports = function(controller) {
+    controller.hears('hello','message',async(bot, message) => {
+    // do something!
+    await bot.reply(message, 'Hello human')
+});
+
+}
