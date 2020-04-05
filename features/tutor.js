55c4036c900e7a5144a66486be0cc6d2d40474d6
Yasser Hawass <yasser.hawass@gmail.com>
2020-04-05 06:25:26 +0200

    first part is done

--- features/tutor.js
+++ features/tutor.js
@@ -6,9 +6,57 @@ module.exports = function(controller) {
     let convo = new BotkitConversation(MY_DIALOG_ID, controller);
     convo.say('Hello!');
     convo.say('Welcome to the world of bots!');
+    convo.ask('What is your name?', async(answer) => { 
+        // do nothing.
+    }, {key: 'name'});
+    
+    convo.ask('What is the leave type?\n1- normal leave\n2- emergency leave\n3-sick leave\n4- other', [ // collect a value with conditional actions
+        {
+            pattern: '1',
+            handler: async function(answer, convo, bot) {
+                await convo.gotoThread('recieved');
+            }
+        },
+        {
+            pattern: '2',
+            handler: async function(answer, convo, bot) {
+                await convo.gotoThread('recieved');
+            }
+        },
+        {
+            pattern: '3',
+            handler: async function(answer, convo, bot) {
+                await convo.gotoThread('recieved');
+            }
+        },
+        {
+            pattern: '4',
+            handler: async function(answer, convo, bot) {
+                convo.setVar('reason', '4');
+            }
+        },
+        {
+            default: true,
+            handler: async(answer, convo, bot, full_message) => {
+                await bot.say('I do not understand your response!');
+                return await convo.repeat();
+            }
+        }
+    ],{key: 'reason'});
+
+    convo.ask("please state it", async(answer, convo, bot) => {
+        // leaving it empty, gave me stall status, which needed Ctrl + C to solve it.
+        await convo.gotoThread('recieved');
+    }, {key: 'reasonx'});
+
+    convo.addMessage('roger!', 'recieved'); // define a 'recieved' thread
+
+    convo.after(async(results, bot) => {        // handle the end of the conversation
+        const name = results.name;
+        const reason = results.reasonx || results.reason;
+    });
     controller.addDialog(convo);
 
-    
 
 
     // HEARS
@@ -32,7 +80,7 @@ module.exports = function(controller) {
         await bot.reply(message, 'Hello back at you, '+global.name);
     });
 
-    
+
     // HEARS - DIALOG
     controller.hears('creata', 'message', async(bot, message) => {
         await bot.beginDialog(MY_DIALOG_ID);
