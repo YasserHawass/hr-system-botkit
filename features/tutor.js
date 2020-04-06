819dd0f71494e4ed60496bcf98de16d14258acaa
Yasser Hawass <yasser.hawass@gmail.com>
2020-04-06 03:54:38 +0200

    errs free 2,3,4 kinda

--- features/tutor.js
+++ features/tutor.js
@@ -49,11 +49,11 @@ module.exports = function(controller) {
 
     convo.ask("please state it", async(answer, convo, bot) => {
         // leaving it empty, gave me stall status, which needed Ctrl + C to solve it.
-        await convo.gotoThread('t_start_date');
+        return await convo.gotoThread('t_start_date');
     }, {key: 'reasonx'});
 
     convo.addMessage('roger!', 't_start_date'); // define a 'recieved' thread
-    convo.addQuestion('What is the start date of your vacation?', [   //  todo Handle with valdiation 
+    convo.addQuestion('What is the start date of your vacation? please write it in y-m-d form (e.g.2020-2-1)', [   //  todo Handle with valdiation 
         {
             pattern: new RegExp(/202\d[\-]\d{1,2}[\-]\d{1,2}/), // please pass it into better date validator function //2020-2-1 and 2025/2/50 are okay here
             handler: async function(answer, convo, bot) {
@@ -63,46 +63,40 @@ module.exports = function(controller) {
         {
             default: true,
             handler: async(answer, convo, bot, full_message) => {
-                await bot.addMessage('I do not understand your response! please write it right!', 't_start_date');
-                // return await convo.addAction('repeat');
                 return await convo.repeat();
             }
         }
     ], {key: 'start_date'}, 't_start_date');
 
-    // convo.before('t_end_date', async(convo, bot) => {
-    //     var GivenDate = convo.vars.sart_date;
-    //     var CurrentDate = new Date();
-    //     GivenDate = new Date(GivenDate);
-    //     console.log(convo.vars.sart_date);
-    //     if (GivenDate > CurrentDate ) {
-    //         console.log("sounds good okay!" + convo.vars.sart_date);
-    //         return await convo.gotoThread('t_end_date');
-    //     }else{
-    //         await bot.addMessage('that was before today, what?' + convo.vars.sart_date, 't_start_date');
-    //         return await convo.gotoThread('t_start_date');
-    //     }
-    // });
+    convo.before('t_end_date', async(convo, bot) => { // abstract it into a func instead of copy-ing pasting it
+        GivenDate = convo.vars.start_date;
+        CurrentDate = new Date();
+        GivenDate = new Date(GivenDate);
+        if (GivenDate > CurrentDate ) {
+            return await convo.gotoThread('t_end_date');
+        }else{
+            console.log("lol that was before dude"); //todo debugging
+            return await convo.repeat();
+        }
+    });
 
-    convo.addQuestion('What is the end date of your vacation?', [  // async(answer) => todo Handle with valdiation & db call
+    convo.addQuestion('What is the end date of your vacation? please write it in y-m-d form (e.g.2020-2-1)', [  // async(answer) => todo Handle with valdiation & db call
         {
             pattern: new RegExp(/202\d[\-]\d{1,2}[\-]\d{1,2}/),
             handler: async function(answer, convo, bot) {
-                console.log("good Date, okay!");// check the db here when done go to next if not repeat please.
+                console.log("good Date, okay!");// todo check the db here when done go to next if not repeat please.
                 await convo.gotoThread('t_alt_emp');
             }
         },
         {
             default: true,
             handler: async(answer, convo, bot, full_message) => {
-                await bot.addMessage('I do not understand your response! please write it right!', 't_end_date');
-                // return await convo.addAction('repeat');
                 return await convo.repeat();
             }
         }
     ], {key: 'end_date'}, 't_end_date');
 
-    convo.addQuestion('what is the alt emp name/id?', async(answer, convo, bot) => {    // async(answer) => todo Handle with valdiation & db call
+    convo.addQuestion('what is the alt emp name/id?', async(answer, convo, bot) => {    // todo Handle with valdiation & db call
     }, {key: 'alt_emp_pinfo'}, 't_alt_emp');
 
 
